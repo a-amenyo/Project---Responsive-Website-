fb0b251fadc11f5105ce6d05d4c75cdb38efa9fc
yoooel <72613221+yoooel@users.noreply.github.com>
2021-04-16 17:56:55 +0000

    contact card added

---
+++ js/contact.js
@@ -0,0 +1,5 @@
+let fName =  document.querySelector("#fname").textContent;
+let fName =  document.querySelector("#fname").textContent;
+console.log(fName);
+let phoneNumber =  document.getElementById("number");
+let submitMessage = document.getElementById("submit"); 
